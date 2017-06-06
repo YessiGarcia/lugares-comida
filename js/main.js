f2d7d8574d32fbddd828ac4722de11bebe54c089
YessiGarcia <aria.801900@gmail.com>
2017-06-06 10:41:55 -0500

    cambio de ubicacion

--- js/main.js
+++ js/main.js
@@ -1,20 +1,76 @@
-function initMap(){
-
-	var laboratoriaMexico = {lat: 19.417704, lng: -99.164886};
-	var restaurante1= {lat:19.4143753,lng:-99.2235053};
-
-	var map = new google.maps.Map(document.getElementById('map'),{
-	zoom: 18,
-	center:laboratoriaMexico
-	});
-
-  	var marker = new google.maps.Marker({
-  	position:laboratoriaMexico,
-  	map: map
-    });
-
-    /*var marker2 = new google.maps.Marker({
-  	position:restaurante1,
-  	map: map
-    });*/
+$(document).ready(cargarPagina);
+// HOISTING
+function cargarPagina() {
+  obtenerUbicacionActual();
+  $(".restaurante").click(cambiarUbicacion);
 }
+
+function obtenerUbicacionActual() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(mostrarPosicionActual);
+  } else {
+    alert("Geolocalización no es soportado en tu navegador");
+  }
+}
+
+function mostrarPosicionActual(posicion) {
+  var latitud = posicion.coords.latitude;
+  var longitud = posicion.coords.longitude;
+
+  var coordenadas = {
+    lat: latitud,
+    lng: longitud
+  };
+
+  mostrarMapa(coordenadas);
+}
+
+// @coordenadas: { lat: <number>, lng: <number> }
+function mostrarMapa(coordenadas) {
+  var map = new google.maps.Map($('.map')[0], {
+    zoom:15,
+    center: coordenadas
+  });
+  var marker = new google.maps.Marker({
+    position: coordenadas,
+    map: map
+  });
+}
+
+function cambiarUbicacion() {
+  var latitud = $(this).data("latitud");
+  var longitud = $(this).data("longitud");
+
+  var coordenadas = {
+    lat: latitud,
+    lng: longitud
+  };
+
+  console.log(coordenadas);
+  mostrarMapa(coordenadas);
+}
+
+var obtenerUbicacion = function (e) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(mostrarPosicion);
+    } else {
+        alert("Actualice su navegador");
+    }
+};
+var mostrarPosicion = function (posicion) {
+    var coordenadas = {
+        lat: posicion.coords.latitude,
+        lng: posicion.coords.longitude
+    };
+    mostrarMapa(coordenadas);
+};
+var mostrarMapa = function (coordenadas) {
+    var map = new google.maps.Map($('.map')[0], {
+      zoom: 17,
+      center: coordenadas
+    });
+    var marker = new google.maps.Marker({
+      position: coordenadas,
+      map: map
+    });
+};
